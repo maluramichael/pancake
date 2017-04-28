6d5180b114f08f6a1a46cf1bd90bf4896fee7ed9
Michael Malura <maluramichael@googlemail.com>
2017-04-28 20:57:12 +0200

    Fix includes

--- src/Menu/imgui_impl_sdl_gl3.cpp
+++ src/Menu/imgui_impl_sdl_gl3.cpp
@@ -6,8 +6,8 @@
 // If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
 // https://github.com/ocornut/imgui
 
-#include "imgui.h"
-#include "imgui_impl_sdl_gl3.h"
+#include <imgui.h>
+#include "../../include/UI/imgui_impl_sdl_gl3.h"
 
 // SDL,GL3W
 #include <SDL.h>
